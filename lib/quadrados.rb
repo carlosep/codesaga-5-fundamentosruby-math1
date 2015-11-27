f08610529bd519237e919072873492b83e7b2632
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-27 12:46:36 -0200

    Soma, Quadrado, Primo... - Concluído

--- lib/quadrados.rb
+++ lib/quadrados.rb
@@ -1,2 +1,23 @@
 class Quadrados
+	require 'prime'
+
+	def initialize(n)
+		@primos = Prime.first n
+	end
+
+	def quadrado_da_soma
+		@resultado = @primos.sum ** 2
+	end
+
+	def soma_dos_quadrados
+		resultado = Array.new
+		@primos.each do |p|
+			resultado.push(p ** 2)
+		end
+		@resultado = resultado.sum
+	end
+
+	def diferenca
+		@resultado = quadrado_da_soma - soma_dos_quadrados
+	end
 end
